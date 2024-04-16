fdab8c53a819be1986f28267e8edd35a9bdfa64a
singhania-sa <147664492+singhania-sa@users.noreply.github.com>
2024-04-16 00:14:55 -0400

    added wishlist to blueprint

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -19,7 +19,7 @@ def create_app():
     app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_root_password.txt').readline().strip()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'edusharehub'  
+    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
@@ -30,22 +30,20 @@ def create_app():
     # Example: localhost:8001
     @app.route("/")
     def welcome():
-        return "<h1>Welcome to EduExchange!</h1>"
+        return "<h1>Welcome to the 3200 boilerplate app</h1>"
 
-    # Import the various Beluprint Objects
-    from src.customers.customers import customers
-    from src.products.products  import products
-    from src.textbooks.textbooks  import textbooks
-    from src.exchange_offers.exchange_offers  import exchange_offers
-    from src.communities.communities  import communities
+    # Import the various Blueprint Objects
+    from src.exchange_offers.exchange_offers import exchange_offers
+    from src.textbooks.textbooks import textbooks
+    from src.online_resources.online_resources import online_resources
+    from src.wishlist.wishlist import wishlist
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
+    app.register_blueprint(exchange_offers, url_prefix='/e')
     app.register_blueprint(textbooks, url_prefix='/t')
-    app.register_blueprint(exchange_offers, url_prefix='/eo')
-    app.register_blueprint(communities, url_prefix='/comm')
+    app.register_blueprint(online_resources, url_prefix='/o')
+    app.register_blueprint(wishlist, url_prefix='/w')
 
     # Don't forget to return the app object
     return app
