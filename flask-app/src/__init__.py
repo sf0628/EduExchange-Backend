7d44a67110f777e9c540f92460344f151bbaa4db
singhania-sa <147664492+singhania-sa@users.noreply.github.com>
2024-04-16 23:16:04 -0400

    updated routes and fixed bugs

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -36,14 +36,14 @@ def create_app():
     from src.exchange_offers.exchange_offers import exchange_offers
     from src.textbooks.textbooks import textbooks
     from src.online_resources.online_resources import online_resources
-    #from src.wishlist.wishlist import wishlist
+    from src.wishlist.wishlist import wishlist
 
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.register_blueprint(exchange_offers, url_prefix='/eo')
     app.register_blueprint(textbooks, url_prefix='/t')
     app.register_blueprint(online_resources, url_prefix='/o')
-    #app.register_blueprint(wishlist, url_prefix='/w')
+    app.register_blueprint(wishlist, url_prefix='/w')
 
     # Don't forget to return the app object
     return app
